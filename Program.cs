6e362cda1c19eb9d0aff94885b69e60e91c16cc9
frmauro <frmauro8@gmail.com>
2021-06-09 15:42:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Core;
+using SalesProductApi;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000", new GrpcChannelOptions { Credentials = ChannelCredentials.Insecure });
+            var client = new ProductServiceProto.ProductServiceProtoClient(channel);
+            var reply = await client.SendProductAsync(new ProductRequest { Id = 1,  Description = "Product 001", Amount = "200", Price = "200", Status = "Active"});
+            Console.WriteLine("Response Product: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# GrpcClientDotNet5
